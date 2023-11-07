d36bc2044390a34d30a02a574346c4361e7e859d
Charles Pletcher <pletcher@users.noreply.github.com>
2023-11-07 11:57:09 +0100

    Highlight lemmata based on w elements

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -71,13 +71,53 @@ async function replaceRawTei(el) {
 	el.replaceChildren(html);
 }
 
+function applyLemma(lemma) {
+	// tei-l@n=61[0]:tei-l@n=61[14]
+	const { selector, text_anchor: textAnchor } = lemma;
+	const [startNode, endNode] = selector.split(':');
+	const [startName, startSpec] = startNode.split('@');
+	const [_startN, _startIdx] = startSpec.split('[');
+	const startN = _startN.replace('n=', '');
+	const startIdx = _startIdx.replace(']', '');
+	const [endName, endSpec] = endNode.split('@');
+	const [_endN, _endIdx] = endSpec.split('[');
+	const endN = _endN.replace('n=', '');
+	const endIdx = _endIdx.replace(']', '');
+
+	const startSelector = `${startName}[n='${startN}'] > tei-w[n='${startIdx}']`
+	const startEl = document.querySelector(startSelector);
+
+	if (startEl) {
+		startEl.classList.add('bg-sky-200')
+	} else {
+		// console.warn(`startEl was null. Selector: ${selector}.`)
+	}
+
+	const endSelector = `${endName}[n='${endN}'] > tei-w[n='${endIdx}']`;
+	const endEl = document.querySelector(endSelector);
+
+	if (endEl) {
+		endEl.classList.add('bg-sky-200')
+	} else {
+		console.warn(`endEl was null. Selector: ${selector} and textAnchor: ${textAnchor}.`)
+	}
+}
+
 export const TEIHook = {
+	lemmas() {
+		return this.el.dataset.lemmas;
+	},
+
 	async mounted() {
 		return replaceRawTei(this.el);
 	},
 
 	async updated() {
-		return replaceRawTei(this.el);
+		await replaceRawTei(this.el);
+
+		const lemmas = JSON.parse(this.lemmas());
+
+		lemmas.forEach(applyLemma);
 	},
 };
 
