26da69f23776e7749a2a846ac900ff6426dad4aa
Charles Pletcher <pletcher@users.noreply.github.com>
2023-11-23 18:04:03 +0100

    I'm not sure why the return type changed here

--- lib/text_server/ingestion/versions.ex
+++ lib/text_server/ingestion/versions.ex
@@ -12,7 +12,7 @@ defmodule TextServer.Ingestion.Versions do
     {:ok, collection} = create_collection()
     {:ok, text_group} = create_text_group(collection)
     {:ok, work} = create_work(text_group)
-    language = create_language()
+    {:ok, language} = create_language()
 
     for xml_file <- xml_files() do
       xml = File.read!(xml_file)
