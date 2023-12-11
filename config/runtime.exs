827d3f20a4e43a8b4f4d0e9f6f5f92a9ff5fb8b7
Charles Pletcher <pletcher@users.noreply.github.com>
2023-12-11 17:17:02 +0100

    Attempt to update config again

--- config/runtime.exs
+++ config/runtime.exs
@@ -62,7 +62,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :text_server, TextServerWeb.Endpoint,
-    # url: [host: host, port: port],
+    url: [host: host, port: port],
     http: [
       ip: {0, 0, 0, 0},
       port: port
