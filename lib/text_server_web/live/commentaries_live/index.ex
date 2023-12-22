117d81aff3764312ed48f9d40425dd9c9dc1eaba
Charles Pletcher <pletcher@users.noreply.github.com>
2023-12-22 13:40:12 +0100

    Start adding gettext files for i18n

--- lib/text_server_web/live/commentaries_live/index.ex
+++ lib/text_server_web/live/commentaries_live/index.ex
@@ -7,7 +7,7 @@ defmodule TextServerWeb.CommentariesLive.Index do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(commentaries: Commentaries.list_public_commentaries())}
+     |> assign(commentaries: Commentaries.list_commentaries())}
   end
 
   def render(assigns) do
@@ -15,21 +15,41 @@ defmodule TextServerWeb.CommentariesLive.Index do
     <div class="px-4 sm:px-6 lg:px-8">
       <div class="sm:flex sm:items-center">
         <div class="sm:flex-auto">
-          <h1 class="text-base font-semibold leading-6 text-gray-900">Bibliography</h1>
+          <h1 class="text-base font-semibold leading-6 text-gray-900"><%= gettext("Bibliography") %></h1>
         </div>
       </div>
       <CoreComponents.table id="bibliography" rows={@commentaries} row_id={fn row -> "commentary_#{row.id}" end}>
-        <:col :let={commentary} label="Author">
-          <%= commentary.creator_first_name %> <%= commentary.creator_last_name %>
+        <:col :let={commentary} label={gettext("Creator(s)")}>
+          <%= creators_to_string(commentary.creators) %>
         </:col>
-        <:col :let={commentary} label="Title"><%= commentary.title %></:col>
-        <:col :let={commentary} label="Publication Date"><%= commentary.publication_date %></:col>
-        <:col :let={_commentary} label="Public Domain?">More data required</:col>
-        <:col :let={commentary} label="Languages">
+        <:col :let={commentary} label={gettext("Title")}><%= commentary.title %></:col>
+        <:col :let={commentary} label={gettext("Publication Date")}><%= commentary.publication_date %></:col>
+        <:col :let={commentary} label={gettext("Public Domain?")}>
+          <%= commentary.public_domain_year < NaiveDateTime.utc_now().year %>
+        </:col>
+        <:col :let={commentary} label={gettext("Languages")}>
           <%= commentary.languages |> Enum.join(", ") %>
         </:col>
       </CoreComponents.table>
     </div>
     """
+  end
+
+  defp creators_to_string(creators) when length(creators) == 1 do
+    creator = creators |> List.first()
+
+    "#{creator.last_name}, #{creator.first_name}"
+  end
+
+  defp creators_to_string(creators) when length(creators) > 1 do
+    [creator | rest] = creators
+
+    s = "#{creator.last_name}, #{creator.first_name}"
+
+    last = List.last(rest)
+
+    rest = (rest -- [last]) |> Enum.map(fn c -> "#{c.first_name} #{c.last_name}" end)
+
+    "#{s}, #{Enum.join(rest, ",")}, and #{last.first_name} #{last.last_name}"
   end
 end
